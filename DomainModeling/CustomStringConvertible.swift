5906a37cf9db2e9eee6f606c2ab4d3d6cac01046
Jacob Alewel <jakealewel@ymail.com>
2015-10-18 17:14:19 -0700

    Added CustomStringConvertible protocol

---
+++ DomainModeling/CustomStringConvertible.swift
@@ -0,0 +1,15 @@
+//
+//  CustomStringConvertible.swift
+//  DomainModeling
+//
+//  Created by Jacob Alewel on 10/18/15.
+//  Copyright © 2015 Jacob Alewel. All rights reserved.
+//
+
+import Foundation
+
+protocol CustomStringConvertible {
+    
+    func description() -> String;
+    
+}
